2eb53596645b7b9a5664512eaa239b6723f4eb8b
Vlad <dunkeroket@gmail.com>
2020-09-10 23:37:42 +0300

    second

---
+++ lab1/Header.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+#include <random>
+#include <locale.h>
+#include <Windows.h>
+
+struct student
+{
+	char famil[20];
+	char name[20], facult[20];
+	int Nomzach;
+} stud[3];
--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <conio.h>
-#include <time.h>
-#include <random>
-
+#define _CRT_SECURE_NO_WARNINGS
+#include "Header.h"
 
 int main()
-
 {   
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	int sum=0;
 	int min=0;
 	int max=0;
@@ -17,22 +15,25 @@ int main()
 	srand(time(NULL));
 	int* Array = NULL;
 	int SIZE;
-	/*генерация массива*/
+	int search = 0;
+
+	/*gen array*/
+	printf("Ведите количество элементов:");
 	scanf("%d", &SIZE);
 	Array = (int*)malloc(sizeof(int)*SIZE);
 	for(int i = 0; i < SIZE; i++) 
 	{
 		Array[i] = rand()%101;
 	}
-     /*Вывод массива*/
+     /*output array*/
 	for(int i = 0; i < SIZE; i++) 
 	{
 		printf("%d ", Array[i]);	
 	}
-
 	max = Array[0];
 	min = Array[0];
-	/*Поиск max и min */
+
+	/*find max and min */
 	for (int i=0; i< SIZE; i++){
 		if(Array[i]<min){
 			min=Array[i];
@@ -43,11 +44,13 @@ int main()
 			nmax=i;
 		}
 	}
-	/*Поиск разницы*/
+
+	/*search sub*/
 	nmax=nmax-nmin;
 	max=max-min;
-	printf("\n rad:%d\n raz:%d\n", abs(nmax),max);
-	/**/
+	printf("\n расстояние:%d\n разность:%d\n", abs(nmax),max);
+
+	/*gen arrayB*/
 	for(int i = 0;i < ROW;i++){
 		for(int j = 0;j < COL;j++){
 			ArrayB[i][j] = rand()%101;
@@ -58,10 +61,45 @@ int main()
 			printf("%d ", ArrayB[i][j]);
 			sum=sum+ ArrayB[i][j];
 		}
-		printf("summ in a row=%d\n", sum);
+		printf("сумма в строке =%d\n", sum);
 		sum = 0;
 		printf("\n");
 	}
-	
+	/*input structure*/
+	int i;
+	for ( i = 0; i < 3; i++)
+	{
+		printf(" Введите фамилию студента:");
+		scanf("%s", stud[i].famil);
+		printf(" Введите имя студента:");
+		scanf("%s", stud[i].name);
+		printf(" Введите название факультета:");
+		scanf("%s", stud[i].facult);
+		printf(" Введите номер зачётной книжки:");
+		scanf(" %d", &stud[i].Nomzach);
+		printf("\n");
+	}
+	/*ouput structure*/
+	for (i = 0; i < 3; i++)
+	{
+			printf("Фамилия студента: %s\n", stud[i].famil);
+			printf("Имя студента: %s\n", stud[i].name);
+			printf("Факультет: %s\n", stud[i].facult);
+			printf("Номер зачётной книжки: %d\n\n", stud[i].Nomzach);
+	}
+	/*serch student*/
+	printf(" Введите номер зачётной книжки:");
+	scanf("%d", &search);
+
+		for (i = 0; i < 3; i++)
+		{
+			if(stud[i].Nomzach == search){
+				printf("Фамилия студента: %s\n", stud[i].famil);
+				printf("Имя студента: %s\n", stud[i].name);
+				printf("Факультет: %s\n", stud[i].facult);
+				printf("Номер зачётной книжки: %d\n", stud[i].Nomzach);
+			}
+
+		}
 	_getch();
 }
