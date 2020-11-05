1c0cf20d595637d8bc3b4b1ad8ee6bf61d7e5840
Vlad <dunkeroket@gmail.com>
2020-11-06 01:10:14 +0300

    HW4

---
+++ LAB4/Header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<locale.h>  
+#include <String.h>
+#include <time.h>
+#include <stack>
+#include <iostream>
---
+++ LAB4/LAB4.cpp
@@ -0,0 +1,125 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "Header.h"
+
+void output(int** Array, int N) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			printf("%d ", Array[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void Gen(int** Array, int N) {
+	printf("\n");
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			Array[i][j] = rand() % 2;
+			Array[j][i] = Array[i][j];
+			if (i == j) {
+				Array[i][j] = 0;
+			}
+		}
+
+	}
+
+	output(Array, N);
+}
+void DFS(int u, int** Array, int* visitedArray, int N) {
+	printf("%d ", u+1);
+
+	visitedArray[u] = 1;
+	for (int uNext = 0; uNext < N; uNext++) {
+		if (Array[u][uNext] && visitedArray[uNext] != 1) {
+			DFS(uNext, Array, visitedArray,N);
+		}
+	}
+}
+void DFSN(int v, int **Array,int* visitedArray, int N)
+{
+	
+	std::stack<int>S;
+	S.push(v);
+	visitedArray[v] = 1;
+	while (!S.empty()) {
+		v = S.top();
+		printf("%d ", v + 1);
+		S.pop();
+		
+		int i = N - 1;
+		while (i!=0) {
+			if (Array[v][i] == 1 && visitedArray[i] != 1) {
+				visitedArray[i] = 1;
+				S.push(i);
+			}
+			i--;
+		}
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "RUS");
+	int M = 0;
+	int N = 0;
+	int u= 0;
+	int** ArrayM1 = NULL;
+	int** ArrayM2 = NULL;
+	int* Visitedarray = NULL;
+	
+	printf("Введите размерность матрицы:");
+	scanf("%d", &N);
+
+	ArrayM1 = (int**)malloc(N * sizeof(int*));
+	for (int i = 0; i < N; i++)
+	{
+		ArrayM1[i] = (int*)malloc(N * sizeof(int));
+	}
+	Gen(ArrayM1, N);
+
+	Visitedarray = (int*)malloc(N * sizeof(int));
+	for (int i = 0; i < N;i++) {
+		Visitedarray[i] = 0;
+	}
+
+	printf("Введите размерность матрицы:");
+	scanf("%d", &M);
+	ArrayM2 = (int**)malloc(M * sizeof(int*));
+	for (int i = 0; i < M; i++)
+	{
+		ArrayM2[i] = (int*)malloc(M * sizeof(int));
+	}
+	Gen(ArrayM2, M);
+
+	printf("\n");
+	for (int i = 0; i < N; i++) {
+		if ( Visitedarray[i] == 0) {
+			
+			DFS(i, ArrayM1, Visitedarray,N);
+		}
+	}
+
+	printf("\n");
+	for (int i = 0; i < N; i++) {
+		Visitedarray[i] = 0;
+	}
+	printf("\n");
+	
+
+		for (int i = 0; i < N; i++) {
+			if (Visitedarray[i] == 0) {
+
+				DFSN(i, ArrayM1, Visitedarray, N);
+			}
+		}
+	
+	
+}
+
+
